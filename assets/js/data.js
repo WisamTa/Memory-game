21fed6ee7d09878c677f22267b294a351dd33e71
WisamTa <wisamtaqaly@gmail.com>
2022-06-05 10:01:11 +0000

    Ad data.js and link js files to index.html

---
+++ assets/js/data.js
@@ -0,0 +1,124 @@
+const cardData = [
+    {
+      name: "Antonio",
+      image: "antonio_giovinazzi.png",
+      id: "antonio"
+    },
+    {
+      name: "Carlos",
+      image: "carlos_sainz.png",
+      id: "carlos"
+    },
+    {
+      name: "Charels",
+      image: "charels_leclerc.png",
+      id: "charels"
+    },
+    {
+      name: "Daniel",
+      image: "daniel_ricciardo.png",
+      id: "daniel"
+    },
+    {
+      name: "Esteban",
+      image: "esteban_ocon.png",
+      id: "esteban"
+    },
+    {
+      name: "Fernando",
+      image: "fernando_alonso.png",
+      id: "fernando"
+    },
+    {
+      name: "George",
+      image: "george_russel.png",
+      id: "george"
+    },
+    {
+      name: "Kimi",
+      image: "kimi_räikkönen.png",
+      id: "kimi"
+    },
+    {
+      name: "Lance",
+      image: "lance_stroll.png",
+      id: "lance"
+    },
+    {
+      name: "Lando",
+      image: "lando_norris.png",
+      id: "lando"
+    },
+    {
+      name: "Lewis",
+      image: "lewis_hamilton.png",
+      id: "lewis"
+    },
+    {
+      name: "Max",
+      image: "max_verstappen.png",
+      id: "max"
+    },
+    {
+      name: "Mick",
+      image: "mick_schumacher.png",
+      id: "mick"
+    },
+    {
+      name: "Nicholas",
+      image: "nicholas_latifi.png",
+      id: "nicholas"
+    },
+    {
+      name: "Nikita",
+      image: "nikita_mazepin.png",
+      id: "nikita"
+    },
+    {
+      name: "Pierre",
+      image: "pierre_gazli.png",
+      id: "pierre"
+    },
+    {
+      name: "Sebastian",
+      image: "sebastian_vettel.png",
+      id: "sebastian"
+    },
+    {
+      name: "Sergio",
+      image: "sergio_perez.png",
+      id: "sergio"
+    },
+    {
+      name: "Valtteri",
+      image: "valtteri_bottas.png",
+      id: "valtteri"
+    },
+    {
+      name: "Yuki",
+      image: "yuki_tysunoda.png",
+      id: "yuki"
+    },
+    
+   ];
+   
+   const gameLevels = {
+    easy: {
+      class: "easy",
+      pairs: 6,
+      twoStar: 6,
+      oneStar: 10
+    },
+    medium: {
+      class: "medium",
+      pairs: 8,
+      twoStar: 10,
+      oneStar: 14
+    },
+    hard: {
+      class: "hard",
+      pairs: 12,
+      twoStar: 16,
+      oneStar: 24
+    }
+   };
